a46399d283749623926198f0435f5d15e3912950
Alonso Parasxidis <alonso_parasxidis@perfaware.com>
2024-02-04 22:36:09 -0800

    feat: Update README

--- terraform/modules/k8s/variables.tf
+++ terraform/modules/k8s/variables.tf
@@ -1,25 +1,31 @@
 variable "ns" {
   type = string
+  description = "Namespace"
 }
 
 variable "app_name" {
   type = string
+  description = "Name of the application"
 }
 
 variable "registry" {
   type = string
+  description = "Name of the registry/repository"
 }
 
 variable "image_tag" {
   type = string
+  description = "Tag of the docker image"
 }
 
 variable "container_port" {
   type = string
   default = 3000
+  description = "Port exposed by the container"
 }
 
 variable "svc_port" {
   type = number
   default = 8080
+  description = "Port exposed by the service"
 }
